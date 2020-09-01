8f23d7029902b2ad14672578353d760cc9f04baa
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2020-09-01 13:34:26 -0500

    recover or create an user

--- tiendadj/tienda/applications/users/views.py
+++ tiendadj/tienda/applications/users/views.py
@@ -1,12 +1,16 @@
-#Third app
+#Third party apps
 from rest_framework.views import APIView
+from firebase_admin import auth
 #Django imports
 from django.shortcuts import render
 #Serializers
 from .serializers import (
     LoginSocialSerializer,
 )
-
+# Models
+from .models import (
+    User,
+)
 # Create your views here.
 from django.views.generic import TemplateView
 
@@ -36,4 +40,20 @@ class GoogleLoginView(APIView):
         serializer_data.is_Valid(raise_exception=True)
         # If data is valid then
         token =  serializer_data.data.get('token_id')
+        # Decoded token by tools fribenase (auth)
+        decoded_token =auth.verify_id_token(token)
+        # Collect all infromation when token is decoded
+        email = decoded_token['email']
+        name = decoded_token['name']
+        avatar = decoded_token['picture']
+        email_verified = decoded_token['email_verified']
+        # Create a user in django app
+        _user, created = User.objects.get_or_create(
+            email=email, #find by email if exist
+            defaults={ #Create if not with following data
+                'full_name':name,
+                'email':email,
+                'is_active':True
+            }
+        )
         return None
