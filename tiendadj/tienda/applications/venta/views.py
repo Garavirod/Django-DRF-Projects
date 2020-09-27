fd0dd7844a17d237a28bcf2c522a108883303ede
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2020-09-27 18:37:30 -0500

    Sale and sale detail was register succesfully

--- tiendadj/tienda/applications/venta/views.py
+++ tiendadj/tienda/applications/venta/views.py
@@ -17,12 +17,14 @@ from django.utils import timezone
 # Serializers
 from . import serializers
 
-# Models
-
+# Sale Models
 from .models import (
     Sale,
-    SaleDetail
+    SaleDetail,
 )
+
+# Product Model
+from applications.producto.models import Product
 
 
 class SaleReportView(ListAPIView):
@@ -60,14 +62,52 @@ class RegisterSaleView(CreateAPIView):
 
         # Access to data previosly validated
         # tipo_recibo = _serialized_data.validated_data['type_invoce']
-        sale = Sale.objects.create(
+
+
+        # This register a sale in BDD acordding to serializer structure
+        _sale = Sale.objects.create(
             date_sale= timezone.now(),
             amount=0,
             count=0,
             type_invoce=_serialized_data.validated_data['type_invoce'],
-            type_payment="", #complete
-            adreese_send="",#complete
-            user="" #coplete
+            type_payment=_serialized_data.validated_data['type_invoce'],
+            adreese_send=_serialized_data.validated_data['adreese_send'],
+            user=self.request.user,
         )
 
-        return Response({'success':'ok'})
+        # Getting sale's products in the register
+        productos = _serialized_data.validated_data['product']
+
+        # for each product in the sale wecreate a sale detail object
+        sales_details = []
+        _amount = 0
+        _count = 0
+
+        for producto in productos:
+            p = Product.objects.get(id=producto['pk']) #getting the product
+            # Obje type sale detail 
+            sale_detail = SaleDetail(
+                sale=_sale, # sale previously registered
+                product=p,
+                count=producto['count'],
+                price_purchase=p.price_purchase,
+                price_sale=p.price_sale
+            )
+            #Adding up the amount
+            _amount += _amount + p.price_sale*producto['count']
+            _count += _count + producto['count']
+
+            #Setting the total amount to Object Sale
+            _sale.amount = _amount
+            _sale.count = _count
+
+            #Saving the data in Sale
+            _sale.save()
+
+            # Adding sale datil to list of sales detail
+            sales_details.append(sale_detail)
+
+        #Bulk create for save Sale detaisl of ech product
+        SaleDetail.objects.bulk_create(sales_details)
+
+        return Response({'success':'sale was registered successful'})
