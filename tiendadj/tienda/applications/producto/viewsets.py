376f26a84d0745527877f7a5646a11340a0b2168
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2020-10-16 20:45:14 -0500

    performed_Created

--- tiendadj/tienda/applications/producto/viewsets.py
+++ tiendadj/tienda/applications/producto/viewsets.py
@@ -1,5 +1,6 @@
 # third party apps
 from rest_framework import viewsets
+from rest_framework.response import Response
 # models
 from .models import Colors, Product
 # Serializers
@@ -28,4 +29,26 @@ class ProductViewset(viewsets.ModelViewSet):
     # Add a pagination class
     pagination_class = PaginationSerializer
 
+    # Perfomed created is a function that can be override to handy data before date be created    
+    def perform_create(self,serializer):
+        # chanege data's serializer in performane, acordin to a model
+        serializer.save(
+            video="https://www.youtube.com/watch?v=gxRq23qVE8A"
+        )
+
+    def list(self,request,*args, **kwargs):
+        queryset = Product.objects.products_by_user(self.request.user)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    # Override method create
+    # def create(self,request):
+    #     # Request data is data that just been created
+    #     print(request.data)
+
+    #     return Response(
+    #         {
+    #             'code':'ok'
+    #         }
+    #     )
 
